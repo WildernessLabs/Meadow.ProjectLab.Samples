6984968250997f28e33a738067f5bbbc7fa5bfef
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-03-24 17:53:31 -0700

    [WifiWeatherview] Add project

---
+++ Source/WifiWeather/MeadowApp.cs
@@ -0,0 +1,90 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Atmospheric;
+using Meadow.Foundation.Sensors.Temperature;
+using Meadow.Gateway.WiFi;
+using System;
+using System.Threading.Tasks;
+using WifiWeather.Services;
+using WifiWeather.ViewModels;
+using WifiWeather.Views;
+
+namespace WifiWeather
+{
+    // public class MeadowApp : App<F7Micro, MeadowApp> <- If you have a Meadow F7v1.*
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        RgbPwmLed onboardLed;
+        WeatherView displayController;
+        Bme680 bme;
+
+        public MeadowApp()
+        {
+            Initialize().Wait();
+
+            Start().Wait();
+        }
+
+        async Task Initialize()
+        {
+            onboardLed = new RgbPwmLed(
+                device: Device,
+                redPwmPin: Device.Pins.OnboardLedRed,
+                greenPwmPin: Device.Pins.OnboardLedGreen,
+                bluePwmPin: Device.Pins.OnboardLedBlue);
+            onboardLed.SetColor(Color.Red);
+
+            var connectionResult = await Device.WiFiAdapter.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD);
+            if (connectionResult.ConnectionStatus != ConnectionStatus.Success)
+            {
+                throw new Exception($"Cannot connect to network: {connectionResult.ConnectionStatus}");
+            }
+
+            bme = new Bme680(Device.CreateI2cBus(), (byte)Bme680.Addresses.Address_0x76);
+            await bme.Read();
+
+            displayController = new WeatherView();
+
+            onboardLed.StartPulse(Color.Green);
+        }
+
+        async Task GetTemperature()
+        {
+            onboardLed.StartPulse(Color.Magenta);
+
+            // Get indoor conditions
+            var roomTemperature = await bme.Read();
+
+            // Get outdoor conditions
+            var outdoorConditions = await WeatherService.GetWeatherForecast();
+
+            onboardLed.StartPulse(Color.Orange);
+
+            // Format indoor/outdoor conditions data
+            var model = new WeatherViewModel(outdoorConditions, roomTemperature.Temperature);
+
+            // Send formatted data to display to render
+            displayController.UpdateDisplay(model);
+
+            onboardLed.StartPulse(Color.Green);
+        }
+
+        async Task Start()
+        {
+            await GetTemperature();
+
+            while (true)
+            {
+                if (DateTime.Now.Minute == 0 && DateTime.Now.Second == 0)
+                {
+                    await GetTemperature();
+                }
+
+                displayController.UpdateDateTime();
+                await Task.Delay(TimeSpan.FromMinutes(1));
+            }
+        }
+    }
+}
---
+++ Source/WifiWeather/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Meadow.Units;
+using System;
+using WifiWeather.Models;
+
+namespace WifiWeather.ViewModels
+{
+    public class WeatherViewModel
+    {
+        public int WeatherCode { get; set; }
+
+        public int OutdoorTemperature { get; set; }
+
+        public int IndoorTemperature { get; set; }
+
+        public WeatherViewModel(WeatherReading outdoorConditions, Temperature? indoorTemperature)
+        {
+            WeatherCode = outdoorConditions.weather[0].id;
+
+            OutdoorTemperature = (int) (outdoorConditions.main.temp - 273);
+
+            IndoorTemperature = (int) indoorTemperature?.Celsius;
+        }
+    }
+}
