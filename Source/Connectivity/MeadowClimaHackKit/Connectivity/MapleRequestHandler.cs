f74614b2291fcb8fbc83f9061d68c4ee6278ce88
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-05-17 00:53:21 -0700

    Add HackKit Clima project as a starting point

---
+++ Source/Connectivity/MeadowClimaHackKit/Connectivity/MapleRequestHandler.cs
@@ -0,0 +1,39 @@
+﻿using CommonContracts.Models;
+using Meadow.Foundation;
+using Meadow.Foundation.Web.Maple.Server;
+using Meadow.Foundation.Web.Maple.Server.Routing;
+using MeadowClimaHackKit.Controller;
+using MeadowClimaHackKit.Database;
+using System.Collections.Generic;
+
+namespace MeadowClimaHackKit.Connectivity
+{
+    public class MapleRequestHandler : RequestHandlerBase
+    {
+        public MapleRequestHandler() { }
+
+        [HttpGet]
+        public void GetTemperatureLogs()
+        {
+            LedController.Instance.SetColor(Color.Magenta);
+
+            var logs = DatabaseManager.Instance.GetTemperatureReadings();
+
+            var data = new List<TemperatureModel>();
+            foreach (var log in logs)
+            {
+                data.Add(new TemperatureModel()
+                {
+                    Temperature = log.TemperatureCelcius?.ToString("00"),
+                    DateTime = log.DateTime.ToString("yyyy-mm-dd hh:mm:ss tt")
+                });
+            }
+
+            LedController.Instance.SetColor(Color.Green);
+
+            Context.Response.ContentType = ContentTypes.Application_Json;
+            Context.Response.StatusCode = 200;
+            Send(data);
+        }
+    }
+}
---
+++ Source/Connectivity/MeadowClimaHackKit/MeadowApp.cs
@@ -0,0 +1,68 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Sensors.Buttons;
+using Meadow.Foundation.Web.Maple.Server;
+using Meadow.Gateway.WiFi;
+using MeadowClimaHackKit.Connectivity;
+using MeadowClimaHackKit.Controller;
+using System;
+using System.Threading.Tasks;
+
+namespace MeadowClimaHackKit
+{
+    // public class MeadowApp : App<F7Micro, MeadowApp> <- If you have a Meadow F7v1.*
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        MapleServer mapleServer;
+        PushButton buttonUp, buttonDown, buttonMenu;
+
+        public MeadowApp()
+        {
+            Initialize();
+        }
+
+        void Initialize() 
+        {
+            LedController.Instance.SetColor(Color.Red);
+
+            //buttonUp = new PushButton(Device, Device.Pins.D03);
+            //buttonDown = new PushButton(Device, Device.Pins.D02);
+            //buttonMenu = new PushButton(Device, Device.Pins.D04);
+
+            //buttonUp.Clicked += (s, e) => DisplayController.Instance.MenuUp();
+            //buttonDown.Clicked += (s, e) => DisplayController.Instance.MenuDown();
+            //buttonMenu.Clicked += (s, e) => DisplayController.Instance.MenuSelect();
+
+            DisplayController.Instance.ShowSplashScreen();
+
+            //InitializeBluetooth();
+            InitializeMaple().Wait();
+        }
+
+        void InitializeBluetooth()
+        {
+            BluetoothServer.Instance.Initialize();
+        }
+
+        async Task InitializeMaple()
+        {
+            DisplayController.Instance.StartWifiConnectingAnimation();
+            
+            var result = await Device.WiFiAdapter.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD);
+            if (result.ConnectionStatus != ConnectionStatus.Success)
+            {
+                throw new Exception($"Cannot connect to network: {result.ConnectionStatus}");
+            }
+
+            DisplayController.Instance.StopWifiConnectingAnimation();
+
+            mapleServer = new MapleServer(Device.WiFiAdapter.IpAddress, 5417, false);
+            mapleServer.Start();
+
+            TemperatureController.Instance.Initialize();
+
+            LedController.Instance.SetColor(Color.Green);
+        }
+    }
+}
