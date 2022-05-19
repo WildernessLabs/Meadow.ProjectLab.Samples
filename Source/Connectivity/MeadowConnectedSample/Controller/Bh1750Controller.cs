11007e9bc7eceaebd9213140a1ea1f76fa168705
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-05-19 01:10:34 -0700

    Add Bh1750Controller

---
+++ Source/Connectivity/MeadowConnectedSample/Controller/Bh1750Controller.cs
@@ -0,0 +1,34 @@
+﻿using Meadow.Foundation.Sensors.Light;
+using Meadow.Hardware;
+using Meadow.Units;
+using System;
+using System.Threading.Tasks;
+
+namespace MeadowConnectedSample.Controller
+{
+    public class Bh1750Controller
+    {
+        private static readonly Lazy<Bh1750Controller> instance =
+            new Lazy<Bh1750Controller>(() => new Bh1750Controller());
+        public static Bh1750Controller Instance => instance.Value;
+
+        Bh1750 bh1750;
+
+        private Bh1750Controller() { }
+
+        public void Initialize(II2cBus i2c)
+        {
+            bh1750 = new Bh1750(
+                    i2cBus: i2c,
+                    measuringMode: Bh1750.MeasuringModes.ContinuouslyHighResolutionMode, // the various modes take differing amounts of time.
+                    lightTransmittance: 1, // lower this to increase sensitivity, for instance, if it's behind a semi opaque window
+                    address: (byte)Bh1750.Addresses.Address_0x23
+                );
+        }
+
+        public Task<Illuminance> Read()
+        {
+            return bh1750.Read();
+        }
+    }
+}
