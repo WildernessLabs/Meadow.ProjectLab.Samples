14b0091b5d51ed08f2f07c3daf866497a2372a40
Adrian Stevens <adrian.stevens@gmail.com>
2022-06-13 16:41:59 -0700

    ProjLab test refactoring - fixes display updates

--- Source/ProjLab_Test/DisplayController.cs
+++ Source/ProjLab_Test/DisplayController.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Meadow.Foundation;
+﻿using Meadow.Foundation;
 using Meadow.Foundation.Graphics;
+using Meadow.Peripherals.Sensors.Hid;
 using Meadow.Units;
 
 namespace HackBoard_Test
@@ -8,80 +8,147 @@ namespace HackBoard_Test
     public class DisplayController
     {
         MicroGraphics graphics;
-        bool buttonClicked = false;
-        MeadowApp.Buttons whichButton;
 
-        public (Temperature? Temperature, RelativeHumidity? Humidity, Pressure? Pressure)? BmeConditions 
-        {
-            get => bmeConditions;
-            set {
-                bmeConditions = value;
-                Render();
-            }
-        } (Temperature? Temperature, RelativeHumidity? Humidity, Pressure? Pressure)? bmeConditions;
 
-        public Illuminance? LightConditions 
+        public (Temperature? Temperature, RelativeHumidity? Humidity, Pressure? Pressure)? AtmosphericConditions
         {
-            get => light;
-            set {
-                light = value;
-                Render();
+            get => atmosphericConditions;
+            set 
+            {
+                atmosphericConditions = value;
+                Update();
             }
-        } Illuminance? light;
+        }
+        (Temperature? Temperature, RelativeHumidity? Humidity, Pressure? Pressure)? atmosphericConditions;
+
+        public Illuminance? LightConditions
+        {
+            get => lightConditions;
+            set
+            {
+                lightConditions = value;
+                Update();
+            }
+        }
+        Illuminance? lightConditions;
+
+        public bool UpButtonState
+        {
+            get => upButtonState;
+            set
+            {
+                upButtonState = value;
+                Update();
+            }
+        }
+        bool upButtonState = false;
+
+
+        public bool DownButtonState
+        {
+            get => downButtonState;
+            set
+            {
+                downButtonState = value;
+                Update();
+            }
+        }
+        bool downButtonState = false;
+
+        public bool LeftButtonState
+        {
+            get => leftButtonState;
+            set
+            {
+                leftButtonState = value;
+                Update();
+            }
+        }
+        bool leftButtonState = false;
+
+        public bool RightButtonState
+        {
+            get => rightButtonState;
+            set
+            {
+                rightButtonState = value;
+                Update();
+            }
+        }
+        bool rightButtonState = false;
+
+
+        bool isUpdating = false;
+        bool needsUpdate = false;
 
         public DisplayController(IGraphicsDisplay display)
         {
-            graphics = new MicroGraphics(display);
-            graphics.Rotation = RotationType._90Degrees;
+            graphics = new MicroGraphics(display)
+            {
+                Rotation = RotationType._90Degrees
+            };
 
             graphics.Clear(true);
         }
 
-        public void Render()
+        public void Update()
         {
+            if (isUpdating)
+            { 
+                needsUpdate = true;
+                return;
+            }
+
+            isUpdating = true;
             graphics.Clear();
             Draw();
             graphics.Show();
+
+            isUpdating = false; 
+
+            if(needsUpdate)
+            {
+                needsUpdate = false;
+                Update();
+            }
         }
 
-        public void DrawButtonClick(MeadowApp.Buttons which)
-        {
-            buttonClicked = true;
-            this.whichButton = which;
-            Render();
-
-            // reset the state machine
-            buttonClicked = false;
-        }
-
-        protected void Draw()
+        void Draw()
         {
             graphics.CurrentFont = new Font12x16();
-            graphics.DrawText(x: 5, y: 5, "hello, Meadow!", WildernessLabsColors.AzureBlue);
+            graphics.DrawText(x: 5, y: 5, "Hello PROJ LAB!", WildernessLabsColors.AzureBlue);
 
-            if (buttonClicked) {
-                graphics.DrawText(x: 5, y: 20, $"{whichButton} clicked.", WildernessLabsColors.ChileanFire);
-            }
 
-            if (BmeConditions is { } conditions) {
+            if (AtmosphericConditions is { } conditions)
+            {
                 if(conditions.Temperature is { } temp)
-                graphics.DrawText(x: 5, y: 40, $"Temp: {temp.Celsius:N2}C", WildernessLabsColors.GalleryWhite);
+                {
+                    graphics.DrawText(x: 5, y:  40, $"Temperature: {temp.Celsius:N1}C", WildernessLabsColors.GalleryWhite);
+                }
+
+                if (conditions.Pressure is { } pressure)
+                {
+                    graphics.DrawText(x: 5, y:  60, $"Pressure:    {pressure.StandardAtmosphere:N1}atm", WildernessLabsColors.GalleryWhite);
+                }
+
+                if (conditions.Humidity is { } humidity)
+                {
+                    graphics.DrawText(x: 5, y:  80, $"Humidity:    {humidity.Percent:N1}%", WildernessLabsColors.GalleryWhite);
+                }
             }
 
-            if (LightConditions is { } light) {
+            if (LightConditions is { } light) 
+            {
                 if (light is { } lightReading)
-                    graphics.DrawText(x: 5, y: 60, $"Lux: {lightReading:N2}Lux", WildernessLabsColors.GalleryWhite);
+                {
+                    graphics.DrawText(x: 5, y: 100, $"Lux:         {lightReading:N0}Lux", WildernessLabsColors.GalleryWhite);
+                }
             }
-        }
 
-        public void UpdateBmeData((Temperature? Temperature, RelativeHumidity? Humidity, Pressure? Pressure) values) 
-        {
-            graphics.DrawText(8, 28, "BME688");
-            graphics.DrawRectangle(15, 48, 200, 56, Color.Black, true);
-            graphics.DrawText(16, 48, $"Temperature: {(int)values.Temperature?.Celsius}°C");
-            graphics.DrawText(16, 68, $"Humidity: {(int)values.Humidity?.Percent}");
-            graphics.DrawText(16, 88, $"Pressure: {(int)values.Pressure?.Millibar}");
-            graphics.Show();
+            graphics.DrawText(x: 5, y: 140, $"Up:    {(UpButtonState ? "pressed" : "released")}", WildernessLabsColors.ChileanFire);
+            graphics.DrawText(x: 5, y: 160, $"Down:  {(DownButtonState ? "pressed" : "released")}", WildernessLabsColors.ChileanFire);
+            graphics.DrawText(x: 5, y: 180, $"Left:  {(LeftButtonState ? "pressed" : "released")}", WildernessLabsColors.ChileanFire);
+            graphics.DrawText(x: 5, y: 200, $"Right: {(RightButtonState ? "pressed" : "released")}", WildernessLabsColors.ChileanFire);
         }
     }
 }
