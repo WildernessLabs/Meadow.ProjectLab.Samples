b8680f0981c6b921e04c7bbca799460372a94c7b
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-01-18 00:45:17 -0800

    Refactor to use IPixelBuffer

--- Source/MagicEightMeadow/MeadowApp.cs
+++ Source/MagicEightMeadow/MeadowApp.cs
@@ -2,6 +2,7 @@
 using Meadow.Devices;
 using Meadow.Foundation;
 using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Graphics.Buffers;
 using Meadow.Foundation.Leds;
 using Meadow.Foundation.Sensors.Accelerometers;
 using Meadow.Units;
@@ -16,10 +17,13 @@ namespace MagicEightMeadow
     // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
     public class MeadowApp : App<F7FeatherV2>
     {
+        IPixelBuffer questionBuffer;
         ProjectLab projectLab;
         RgbPwmLed onboardLed;
         MicroGraphics graphics;
         bool isAnswering = false;
+
+        string GetQuestionFilename => "MagicEightMeadow.m8b_question.jpg";
 
         public override Task Initialize()
         {
@@ -35,6 +39,8 @@ namespace MagicEightMeadow
             graphics = new MicroGraphics(projectLab.Display);
             graphics.Rotation = RotationType._90Degrees;
 
+            questionBuffer = LoadJpeg(LoadResource(GetQuestionFilename));
+
             var consumer = Bmi270.CreateObserver(
                 handler: result => MotionSensorHandler(result),
                 filter: result => MotionSensorFilter(result));
@@ -47,20 +53,17 @@ namespace MagicEightMeadow
 
         private async void MotionSensorHandler (IChangeResult<(Acceleration3D? a3D, AngularVelocity3D? v3D, Temperature? t)> e)
         {
-
             if (isAnswering)
                 return;
             isAnswering = true;
 
             onboardLed.SetColor(Color.Orange);
 
-            var rand = new Random();
-
-            DisplayJPG(rand.Next(1, 21));
+            DisplayAnswer();
 
             await Task.Delay(TimeSpan.FromSeconds(5));
 
-            DisplayJPG(21);
+            DisplayQuestion();
 
             onboardLed.SetColor(Color.Green);
 
@@ -72,42 +75,29 @@ namespace MagicEightMeadow
             return e.New.v3D.Value.Y.DegreesPerSecond > 0.75;
         }
 
-        void DisplayJPG(int answerNumber)
+        void DisplayQuestion()
         {
-            var jpgData = LoadResource(answerNumber);
-            var decoder = new JpegDecoder();
-            var jpg = decoder.DecodeJpeg(jpgData);
-
-            int x = 0;
-            int y = 0;
-            byte r, g, b;
-
-            for (int i = 0; i < jpg.Length; i += 3)
-            {
-                r = jpg[i];
-                g = jpg[i + 1];
-                b = jpg[i + 2];
-
-                graphics.DrawPixel(x, y, Color.FromRgb(r, g, b));
-
-                x++;
-                if (x % decoder.Width == 0)
-                {
-                    y++;
-                    x = 0;
-                }
-            }
-
+            graphics.DrawBuffer(0, 0, questionBuffer);
             graphics.Show();
         }
 
-        byte[] LoadResource(int answerNumber)
+        void DisplayAnswer() 
+        {
+            var rand = new Random();
+
+            var buffer = LoadJpeg(LoadResource(GetAnswerFilename(rand.Next(1,21))));
+
+            graphics.DrawBuffer(0, 0,buffer);
+            graphics.Show();
+        }
+
+        string GetAnswerFilename(int answerNumber) => $"MagicEightMeadow.m8b_{answerNumber.ToString("00")}.jpg";
+
+        byte[] LoadResource(string fileName)
         {
             var assembly = Assembly.GetExecutingAssembly();
 
-            var resourceName = $"MagicEightMeadow.m8b_{answerNumber.ToString("00")}.jpg";
-
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (Stream stream = assembly.GetManifestResourceStream(fileName))
             {
                 using (var ms = new MemoryStream())
                 {
@@ -117,11 +107,19 @@ namespace MagicEightMeadow
             }
         }
 
+        IPixelBuffer LoadJpeg(byte[] jpgData)
+        {
+            var decoder = new JpegDecoder();
+            var jpg = decoder.DecodeJpeg(jpgData);
+
+            return new BufferRgb888(decoder.Width, decoder.Height, jpg);
+        }
+
         public override Task Run()
         {
             Console.WriteLine("Run...");
 
-            DisplayJPG(21);
+            DisplayQuestion();
             projectLab.MotionSensor.StartUpdating(TimeSpan.FromSeconds(1));
 
             return base.Run();
