b2487db8c58ab156b1157597b702869f501dc5b3
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-04-10 15:23:46 -0700

    Update Project Lab IoTHub Sample

---
+++ Source/MeadowAzureIoTHub/Azure/AmqpController.cs
@@ -0,0 +1,102 @@
+﻿using Amqp;
+using Meadow;
+using Meadow.Units;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeadowAzureIoTHub.Azure
+{
+    /// <summary>
+    /// You'll need to create an IoT Hub - https://learn.microsoft.com/en-us/azure/iot-hub/iot-hub-create-through-portal
+    /// Create a device within your IoT Hub
+    /// And then generate a SAS token - this can be done via the Azure CLI 
+    /// 
+    /// Example
+    /// az iot hub generate-sas-token
+    /// --hub-name HUB_NAME 
+    /// --device-id DEVICE_ID 
+    /// --resource-group RESOURCE_GROUP 
+    /// --login [Open Shared access policies -> Select iothubowner -> copy Primary connection string]
+    /// </summary>
+    public class AmqpController
+    {
+        private static readonly Random rand = new Random();
+
+        private const string HubName = Secrets.HUB_NAME;
+        private const string SasToken = Secrets.SAS_TOKEN;
+        private const string DeviceId = Secrets.DEVICE_ID;
+
+        private Connection connection;
+        private SenderLink sender;
+        private ReceiverLink receiver;
+
+        public AmqpController() { }
+
+        public async Task Initialize()
+        {
+            string hostName = HubName + ".azure-devices.net";
+            string userName = DeviceId + "@sas." + HubName;
+            string senderAddress = "devices/" + DeviceId + "/messages/events";
+            string receiverAddress = "devices/" + DeviceId + "/messages/deviceBound";
+
+            Resolver.Log.Info("Create connection factory...");
+            var factory = new ConnectionFactory();
+
+            Resolver.Log.Info("Create connection ...");
+            connection = await factory.CreateAsync(new Address(hostName, 5671, userName, SasToken));
+
+            Resolver.Log.Info("Create session ...");
+            var session = new Session(connection);
+
+            Resolver.Log.Info("Create SenderLink ...");
+            sender = new SenderLink(session, "send-link", senderAddress);
+
+            Resolver.Log.Info("Create ReceiverLink ...");
+            receiver = new ReceiverLink(session, "receive-link", receiverAddress);
+            receiver.Start(100, OnMessage);
+        }
+
+        public Task SendEnvironmentalReading((Temperature? Temperature, RelativeHumidity? Humidity, Pressure? Pressure, Resistance? GasResistance) reading)
+        {
+            try
+            {
+                Resolver.Log.Info("Create payload");
+                string messagePayload = $"{{\"Temperature\":{reading.Temperature.Value.Celsius}," +
+                    $"\"Humidity\":{reading.Humidity.Value.Percent}," +
+                    $"\"Pressure\":{reading.Pressure.Value.Millibar}}}";
+
+                Resolver.Log.Info("Create message");
+                var message = new Message(Encoding.UTF8.GetBytes(messagePayload));
+                message.ApplicationProperties = new Amqp.Framing.ApplicationProperties();
+
+                Resolver.Log.Info("Send message");
+                sender.Send(message, null, null);
+
+                Resolver.Log.Info($"*** DATA SENT - Temperature - {reading.Temperature.Value.Celsius}, Humidity - {reading.Humidity.Value.Percent}, Pressure - {reading.Pressure.Value.Millibar} ***");
+            }
+            catch (Exception ex)
+            {
+                Resolver.Log.Info($"-- D2C Error - {ex.Message} --");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static void OnMessage(IReceiverLink receiver, Message message)
+        {
+            Resolver.Log.Info("Message received");
+
+            try
+            {
+                //double.TryParse((string)message.ApplicationProperties["setlat"], out latitude);
+                //double.TryParse((string)message.ApplicationProperties["setlon"], out longitude);
+                //Resolver.Log.Info($"== Received new Location setting: Lat - {latitude}, Lon - {longitude} ==");
+            }
+            catch (Exception ex)
+            {
+                Resolver.Log.Info($"-- C2D Error - {ex.Message} --");
+            }
+        }
+    }
+}
--- Source/MeadowAzureIoTHub/MeadowApp.cs
+++ Source/MeadowAzureIoTHub/MeadowApp.cs
@@ -1,13 +1,11 @@
-﻿using Amqp;
-using Meadow;
+﻿using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation;
 using Meadow.Foundation.Leds;
 using Meadow.Hardware;
+using MeadowAzureIoTHub.Azure;
 using MeadowAzureIoTHub.Views;
 using System;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace MeadowAzureIoTHub
@@ -16,32 +14,8 @@ namespace MeadowAzureIoTHub
     public class MeadowApp : App<F7FeatherV2>
     {
         RgbPwmLed onboardLed;
-
-        private static readonly Random rand = new Random();
-
-        /// <summary>
-        /// You'll need to create an IoT Hub - https://learn.microsoft.com/en-us/azure/iot-hub/iot-hub-create-through-portal
-        /// Create a device within your IoT Hub
-        /// And then generate a SAS token - this can be done via the Azure CLI 
-        /// 
-        /// Example
-        /// az iot hub generate-sas-token
-        /// --hub-name HUB_NAME 
-        /// --device-id DEVICE_ID 
-        /// --resource-group RESOURCE_GROUP 
-        /// --login [Open Shared access policies -> Select iothubowner -> copy Primary connection string
-        /// </summary>
-
-        const string HubName = Secrets.HUB_NAME;
-        const string SasToken = Secrets.SAS_TOKEN;
-        const string DeviceId = Secrets.DEVICE_ID;
-
-        // Lat/Lon Points
-        static double latitude = 49.246292;
-        static double longitude = -123.116226;
-        const double radius = 6378;
-
-        static readonly bool traceOn = false;
+        IProjectLabHardware projectLab;
+        AmqpController amqpController;
 
         public override async Task Initialize()
         {
@@ -54,10 +28,13 @@ namespace MeadowAzureIoTHub
 
             try
             {
+                amqpController = new AmqpController();
+
                 var wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
                 wifi.NetworkConnected += NetworkConnected;
 
-                var projectLab = ProjectLab.Create();
+                projectLab = ProjectLab.Create();
+                projectLab.EnvironmentalSensor.Updated += EnvironmentalSensorUpdated;
 
                 DisplayController.Instance.Initialize(projectLab.Display);
                 DisplayController.Instance.ShowSplashScreen();
@@ -69,127 +46,21 @@ namespace MeadowAzureIoTHub
             }
         }
 
-        private void NetworkConnected(INetworkAdapter sender, NetworkConnectionEventArgs args)
+        private async void NetworkConnected(INetworkAdapter sender, NetworkConnectionEventArgs args)
         {
             Console.WriteLine("Connected...");
-            onboardLed.SetColor(Color.Green);
             DisplayController.Instance.StopConnectingAnimation();
 
-            latitude = 49.246292;
-            longitude = -123.116226;
+            await amqpController.Initialize();
 
-            //IoTHubDataTest();
+            projectLab.EnvironmentalSensor.StartUpdating(TimeSpan.FromSeconds(5));
+
+            onboardLed.SetColor(Color.Green);
         }
 
-        void IoTHubDataTest()
+        private void EnvironmentalSensorUpdated(object sender, IChangeResult<(Meadow.Units.Temperature? Temperature, Meadow.Units.RelativeHumidity? Humidity, Meadow.Units.Pressure? Pressure, Meadow.Units.Resistance? GasResistance)> e)
         {
-            Console.WriteLine("Amqp setup");
-
-            Thread.Sleep(1000);
-
-            // setup AMQP
-            Trace.TraceLevel = TraceLevel.Frame | TraceLevel.Information;
-            // enable trace
-            Trace.TraceListener = WriteTrace;
-            Connection.DisableServerCertValidation = false;
-
-            SendLatLongData();
-        }
-
-        private async Task SendLatLongData()
-        {
-            try
-            {
-                // parse Azure IoT Hub Map settings to AMQP protocol settings
-                string hostName = HubName + ".azure-devices.net";
-                string userName = DeviceId + "@sas." + HubName;
-                string senderAddress = "devices/" + DeviceId + "/messages/events";
-                string receiverAddress = "devices/" + DeviceId + "/messages/deviceBound";
-
-                Console.WriteLine("Create connection factory...");
-                var factory = new ConnectionFactory();
-                Console.WriteLine("Create connection ...");
-                var connection = await factory.CreateAsync(new Address(hostName, 5671, userName, SasToken));
-                Console.WriteLine("Create session ...");
-                var session = new Session(connection);
-                Console.WriteLine("Create SenderLink ...");
-                var sender = new SenderLink(session, "send-link", senderAddress);
-                Console.WriteLine("Create ReceiverLink ...");
-                var receiver = new ReceiverLink(session, "receive-link", receiverAddress);
-                receiver.Start(100, OnMessage);
-
-                while (true)
-                {
-                    // update the location data
-                    Console.WriteLine("Update location data");
-
-                    UpdateMockDestination();
-
-                    Console.WriteLine("Create payload");
-                    string messagePayload = $"{{\"Latitude\":{latitude},\"Longitude\":{longitude}}}";
-
-                    // compose message
-                    Console.WriteLine("Create message");
-                    var message = new Message(Encoding.UTF8.GetBytes(messagePayload));
-                    message.ApplicationProperties = new Amqp.Framing.ApplicationProperties();
-
-                    // send message with the new Lat/Lon
-                    Console.WriteLine("Send message");
-                    sender.Send(message, null, null);
-
-                    // data sent
-                    Console.WriteLine($"*** DATA SENT - Lat - {latitude}, Lon - {longitude} ***");
-
-                    // wait before sending the next position update
-                    Console.WriteLine("Sleep");
-                    Thread.Sleep(2000);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"-- D2C Error - {ex.Message} --");
-            }
-        }
-
-        private static void OnMessage(IReceiverLink receiver, Message message)
-        {
-            Console.WriteLine("Message received");
-
-            try
-            {   // command received 
-                double.TryParse((string)message.ApplicationProperties["setlat"], out latitude);
-                double.TryParse((string)message.ApplicationProperties["setlon"], out longitude);
-                Console.WriteLine($"== Received new Location setting: Lat - {latitude}, Lon - {longitude} ==");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"-- C2D Error - {ex.Message} --");
-            }
-        }
-
-        static void WriteTrace(TraceLevel level, string format, params object[] args)
-        {
-            if (traceOn)
-            {
-                Console.WriteLine(Fx.Format(format, args));
-            }
-        }
-
-        // Starting at the last Lat/Lon move along the bearing and for the distance to reset the Lat/Lon at a new point...
-        public static void UpdateMockDestination()
-        {
-            // Get a random Bearing and Distance...
-            double distance = rand.Next(10);     // Random distance from 0 to 10km...
-            double bearing = rand.Next(360);     // Random bearing from 0 to 360 degrees...
-
-            double lat1 = latitude * (Math.PI / 180);
-            double lon1 = longitude * (Math.PI / 180);
-            double brng = bearing * (Math.PI / 180);
-            double lat2 = Math.Asin(Math.Sin(lat1) * Math.Cos(distance / radius) + Math.Cos(lat1) * Math.Sin(distance / radius) * Math.Cos(brng));
-            double lon2 = lon1 + Math.Atan2(Math.Sin(brng) * Math.Sin(distance / radius) * Math.Cos(lat1), Math.Cos(distance / radius) - Math.Sin(lat1) * Math.Sin(lat2));
-
-            latitude = lat2 * (180 / Math.PI);
-            longitude = lon2 * (180 / Math.PI);
+            amqpController.SendEnvironmentalReading(e.New);
         }
     }
 }
