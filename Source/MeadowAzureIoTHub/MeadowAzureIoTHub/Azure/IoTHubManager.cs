a46d4f1de553a3c5451ba6e42c073aaf854564ba
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-08-10 12:44:03 -0700

    Update Azure IoT Hub Project

--- Source/MeadowAzureIoTHub/MeadowAzureIoTHub/Azure/IoTHubManager.cs
+++ Source/MeadowAzureIoTHub/MeadowAzureIoTHub/Azure/IoTHubManager.cs
@@ -12,14 +12,10 @@ namespace MeadowAzureIoTHub.Azure
     /// You'll need to create an IoT Hub - https://learn.microsoft.com/en-us/azure/iot-hub/iot-hub-create-through-portal
     /// Create a device within your IoT Hub
     /// And then generate a SAS token - this can be done via the Azure CLI 
-    /// 
-    /// Example
-    /// az iot hub generate-sas-token
-    /// --hub-name HUB_NAME 
-    /// --device-id DEVICE_ID 
-    /// --resource-group RESOURCE_GROUP 
-    /// --login [Open Shared access policies -> Select iothubowner -> copy Primary connection string]
     /// </summary>
+    /*
+     az iot hub generate-sas-token --hub-name tank-iot-hub --device-id meadow-project-lab --resource-group tank-resource-group --login HostName=tank-iot-hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=dLQrcFokPCx0NSNcWXK+CqCBRpytAWZWGCMz7zi09pE=
+    */
     public class IotHubManager
     {
         private const string HubName = Secrets.HUB_NAME;
