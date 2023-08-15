052284f42159b0d51608727c2a5f6f9b723eb2c0
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-08-15 16:24:50 -0700

    Update MicroLayout project

--- Source/MicroLayoutMenu/MeadowApp.cs
+++ Source/MicroLayoutMenu/MeadowApp.cs
@@ -1,5 +1,7 @@
 ﻿using Meadow;
 using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Graphics;
 using Meadow.Foundation.Graphics.MicroLayout;
 using System.Threading.Tasks;
 
@@ -8,10 +10,59 @@ namespace MicroLayoutMenu
     // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
     public class MeadowApp : App<F7CoreComputeV2>
     {
+        private IProjectLabHardware _projectLab;
         private DisplayScreen _screen;
         private Menu _menu;
 
         public override Task Initialize()
+        {
+            _projectLab = ProjectLab.Create();
+
+            _screen = new DisplayScreen(_projectLab.Display, RotationType._270Degrees);
+
+            return base.Initialize();
+        }
+
+        void ShowDemoScreen()
+        {
+            var image = Image.LoadFromResource("MicroLayoutMenu.img_meadow.bmp");
+
+            _screen.Controls.Add(
+                new DisplayBox(0, 0, _screen.Width, _screen.Height)
+                {
+                    ForeColor = Color.White
+                },
+                new DisplayLabel(15, 20, 290, 40)
+                {
+                    Text = "Welcome to MicroLayout!",
+                    TextColor = Color.Black,
+                    BackColor = Color.FromHex("#C9DB31"),
+                    Font = new Font12x20(),
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                },
+                new DisplayImage(90, 74, 140, 90, image)
+                {
+                    BackColor = Color.FromHex("#23ABE3"),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                },
+                new DisplayButton(30, 178, 120, 40)
+                {
+                    Text = "Button 1",
+                    TextColor = Color.Black,
+                    Font = new Font12x20(),
+                },
+                new DisplayButton(170, 178, 120, 40)
+                {
+                    Text = "Button 2",
+                    TextColor = Color.White,
+                    ShadowColor = Color.FromHex("#555555"),
+                    Font = new Font12x20(),
+                });
+        }
+
+        void ShowMenuScreen()
         {
             var menuItems = new string[]
                 {
@@ -23,20 +74,18 @@ namespace MicroLayoutMenu
                     "Item F",
                 };
 
-            var projLab = ProjectLab.Create();
-
-            _screen = new DisplayScreen(projLab.Display, Meadow.Foundation.Graphics.RotationType._270Degrees);
-
             _menu = new Menu(menuItems, _screen);
 
-            projLab.UpButton.Clicked += (s, e) => _menu.Up();
-            projLab.DownButton.Clicked += (s, e) => _menu.Down();
-
-            return base.Initialize();
+            _projectLab.UpButton.Clicked += (s, e) => _menu.Up();
+            _projectLab.DownButton.Clicked += (s, e) => _menu.Down();
         }
 
         public override Task Run()
         {
+            ShowDemoScreen();
+
+            //ShowMenuScreen();
+
             return base.Run();
         }
     }
