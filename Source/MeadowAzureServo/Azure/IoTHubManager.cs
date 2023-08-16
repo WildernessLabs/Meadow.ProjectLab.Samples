29c1a296865ebc8e0148f987fb241f83b4f55cb8
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-08-16 12:23:40 -0700

    Minor cleanup

--- Source/MeadowAzureServo/Azure/IoTHubManager.cs
+++ Source/MeadowAzureServo/Azure/IoTHubManager.cs
@@ -10,14 +10,10 @@ namespace MeadowAzureServo.Azure
     /// You'll need to create an IoT Hub - https://learn.microsoft.com/en-us/azure/iot-hub/iot-hub-create-through-portal
     /// Create a device within your IoT Hub
     /// And then generate a SAS token - this can be done via the Azure CLI 
-    /// 
-    /// Example
-    /// az iot hub generate-sas-token
-    /// --hub-name HUB_NAME 
-    /// --device-id DEVICE_ID 
-    /// --resource-group RESOURCE_GROUP 
-    /// --login [Open Shared access policies -> Select iothubowner -> copy Primary connection string]
     /// </summary>
+    /*
+     az iot hub generate-sas-token --hub-name HUB_NAME --device-id DEVICE_ID --resource-group RESOURCE_GROUP --login [Open Shared access policies -> Select iothubowner -> copy Primary connection string]
+    */
     public class IoTHubManager
     {
         private const string HubName = Secrets.HUB_NAME;
