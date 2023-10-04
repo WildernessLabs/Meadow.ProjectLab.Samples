36f9271ad1e1c6cec24043923752f87914866a8c
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-10-04 11:36:26 -0700

    Add SAS token command

--- Source/MeadowAzureIoTHub/MeadowAzureIoTHub/Secrets.cs
+++ Source/MeadowAzureIoTHub/MeadowAzureIoTHub/Secrets.cs
@@ -24,6 +24,8 @@
 
         /// <summary>
         /// example "SharedAccessSignature sr=MeadowIoTHub ..... "
+        /// 
+        /// az iot hub generate-sas-token --hub-name HUB_NAME --device-id DEVICE_ID --resource-group RESOURCE_GROUP --login [Open Shared access policies -> Select iothubowner -> copy Primary connection string]
         /// </summary>
         public const string SAS_TOKEN = "SAS_TOKEN";
     }
