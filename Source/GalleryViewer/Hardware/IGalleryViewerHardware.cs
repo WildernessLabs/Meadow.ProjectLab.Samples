06381facb0f1cafbd497507da2aec72621555972
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-11-28 10:46:48 -0800

    Update GalleryViewer sample

---
+++ Source/GalleryViewer/Hardware/IGalleryViewerHardware.cs
@@ -0,0 +1,19 @@
+﻿using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Leds;
+using Meadow.Peripherals.Sensors.Buttons;
+
+namespace GalleryViewer.Hardware
+{
+    internal interface IGalleryViewerHardware
+    {
+        public IGraphicsDisplay Display { get; }
+
+        public IButton RightButton { get; }
+
+        public IButton LeftButton { get; }
+
+        public RgbPwmLed RgbPwmLed { get; }
+
+        public void Initialize();
+    }
+}
